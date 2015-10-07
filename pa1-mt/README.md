b6bebb17bfe35b3f6691a043616337e2821a18d7
Wenbo Vincent Zhang <wenbo6@corn27.stanford.edu>
2015-10-06 21:23:31 -0700

    first feature, f.targetPhrase.size()

---
+++ pa1-mt/README.md
@@ -0,0 +1,8 @@
+First, follow the PA1 instruction to setup environment.
+source setup.sh
+
+To modify My features, just play with 
+vi ./java/src/cs224n/MyFeaturizer.java
+
+To rebuild and train the system to get a new BLEU, just run
+sh run.sh
--- pa1-mt/run.sh
+++ pa1-mt/run.sh
@@ -1,5 +1,16 @@
-cd ~/cs224n/pa1-mt/java
-ant
+#!/bin/bash
 
-cd ../system
-phrasal.sh cs224n.vars 2,4-5 myFeature.ini myfeature
+if [ "$1" == "new" ]; then
+  cd ~/cs224n/pa1-mt/java
+  ant
+  cd ~/cs224n/pa1-mt/system
+  phrasal.sh cs224n.vars 2,4-5 myFeature.ini myfeature
+else
+  cd ~/cs224n/pa1-mt/system
+fi
+
+echo $'\nBLEU:'
+head newstest2012.BLEU
+
+echo $'\nFEATURES:'
+java edu.stanford.nlp.mt.tools.PrintWeights newstest2011.myfeature.online.final.binwts
