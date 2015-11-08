e3d2f33ac1094d6e354287c135fc8221c8ae4b7d
infinitespace <vincent.zhang.us@gmail.com>
2015-11-08 14:30:35 -0800

    update rulebase

--- pa3/java/src/cs224n/corefsystems/RuleBased.java
+++ pa3/java/src/cs224n/corefsystems/RuleBased.java
@@ -1,85 +1,159 @@
 package cs224n.corefsystems;
 
-import java.util.Collection;
-import java.util.List;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.*;
-
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import cs224n.coref.ClusteredMention;
 import cs224n.coref.Document;
-import cs224n.coref.*;
+import cs224n.coref.Entity;
+import cs224n.coref.Gender;
+import cs224n.coref.Mention;
+import cs224n.coref.Name;
+import cs224n.coref.Pronoun;
+import cs224n.coref.Util;
+import cs224n.coref.Pronoun.Speaker;
+import cs224n.util.CounterMap;
 import cs224n.util.Pair;
 
 public class RuleBased implements CoreferenceSystem {
-    private HashSet<String> handwriten;
-    private String getKeyFromMentionPair(Mention m1, Mention m2){
-        String token1 = m1.headToken().word();
-        String token2 = m2.headToken().word();
-        String[] tokens = {token1, token2};
-        Arrays.sort(tokens);
-        return tokens[0] + tokens[1];
+
+    public void train(Collection<Pair<Document, List<Entity>>> trainingData) {
     }
 
-	@Override
-	public void train(Collection<Pair<Document, List<Entity>>> trainingData) {
-		handwriten = new HashSet<String>();
-        for(Pair<Document, List<Entity>> pair : trainingData){
-          List<Entity> clusters = pair.getSecond();
-          for(Entity e : clusters){
-            // System.out.println(e);
-            for(Pair<Mention, Mention> mentionPair : e.orderedMentionPairs()){
-                handwriten.add(getKeyFromMentionPair(mentionPair.getFirst(), mentionPair.getSecond()));
-            }
-          }
-        }
-	}
+    @Override
+    public List<ClusteredMention> runCoreference(Document doc) {
 
-	@Override
-	public List<ClusteredMention> runCoreference(Document doc) {
-        ArrayList<ClusteredMention> mentions = new ArrayList<ClusteredMention>(); // result
-        
-        HashMap<String, Entity> mentionHeadPair = new HashMap<String, Entity>(); // header pair from training set
-        HashMap<String, Entity> head2Entity = new HashMap<String, Entity>(); // direct match by head
+        List<ClusteredMention> mentions = new ArrayList<ClusteredMention>();
+        Map<String, Entity> clusters = new HashMap<String, Entity>();
+        List<ClusteredMention> tmp_mentions = new ArrayList<ClusteredMention>();
 
-        for (Mention m1 : doc.getMentions()) {
-            String token1 = m1.headToken().word();
-            // direct match by head
-            if(head2Entity.containsKey(token1)){
-                mentions.add(m1.markCoreferent(head2Entity.get(token1)));
-            }
-            else{
-                boolean findPair = false;
-                String key = "";
-                for (Mention m2 : doc.getMentions()) {
-                    key = getKeyFromMentionPair(m1, m2);
-                    if(handwriten.contains(key)){
-                        findPair = true;
+        for (Mention m : doc.getMentions()) {
+            boolean flag = false;
+            for (ClusteredMention cm : tmp_mentions) {
+                for (Mention m2 : cm.entity.mentions) {
+                    if (sameMention(m, m2)) {
+                        mentions.add(m.markCoreferent(cm.entity));
+                        flag = true;
                         break;
                     }
                 }
-                // header pair from training set
-                if(findPair){
-                    if(mentionHeadPair.containsKey(key)){
-                        mentions.add(m1.markCoreferent(mentionHeadPair.get(key)));
-                        head2Entity.put(token1, mentionHeadPair.get(key));
-                    }
-                    else{
-                        ClusteredMention newCluster = m1.markSingleton();
-                        mentions.add(newCluster);
-                        mentionHeadPair.put(key, newCluster.entity);
-                        head2Entity.put(token1, newCluster.entity);
-                    }
-                }
-                else{
-                    ClusteredMention newCluster = m1.markSingleton();
-                    mentions.add(newCluster);
-                    head2Entity.put(token1, newCluster.entity);
-                }
+                if (flag) break;
             }
-        }
+            if (flag == false) {
+                ClusteredMention clm = m.markSingleton();
+                tmp_mentions.add(clm);
+                mentions.add(clm);
+            }
+
+        }      
         return mentions;
+
+    }
+
+    boolean sameMention(Mention m1, Mention m2) {
+        if (exactMatch(m1, m2))
+            return true;
+        if (headMatch(m1, m2))
+            return true;
+
+        if (!sameGenderOrUnknown(m1, m2))
+            return false;
+        if (!sameNumberOrUnknown(m1, m2))
+            return false;
+        if (Pronoun.isSomePronoun(m2.gloss()) && m1.sentence.equals(m2.sentence)
+                && ((m2.headWordIndex - m1.headWordIndex) > 0) && ((m2.headWordIndex - m1.headWordIndex) < 5))
+            return true;
+        if (!sameNE(m1, m2)) {
+            return false;
+        }
+
+        if (pronouAndNameAgree(m1, m2)) {
+            return true;
+        }
+
+        if (!sameLemma(m1, m2)) {
+            return false;
+        }
+
+        if (contains(m1, m2)) {
+            return true;
+        }
+        return true;
+
+    }
+
+    boolean pronouAndNameAgree(Mention m1, Mention m2) {
+        Pronoun p1 = Pronoun.valueOrNull(m1.gloss());// getPronoun(m1.gloss());
+
+        // if (p1 == null && m2.gloss().equals("it"))
+        // return true;
+
+        Pronoun p2 = Pronoun.valueOrNull(m2.gloss());// getPronoun(m2.gloss());
+        Name n1 = Name.get(m1.gloss());
+        Name n2 = Name.get(m2.gloss());
+
+        if (n1 != null && n2 != null && n1.gloss.equals(n2.gloss))
+            return true;
+
+        if (p1 == null || p2 == null)
+            return false;
+        if (p1.speaker == p2.speaker && p1.plural == p2.plural) {
+            // System.out.println(p1.speaker);
+            return true;
+        }
+
+        return false;
+    }
+
+    boolean contains(Mention m1, Mention m2) {
+        if (m1.gloss().contains(m2.gloss()) || m2.gloss().contains(m1.gloss()))
+            return true;
+        return false;
+    }
+
+    boolean exactMatch(Mention m1, Mention m2) {
+        if (m1.gloss().equals(m2.gloss()))
+            return true;
+        return false;
+    }
+
+    boolean headMatch(Mention m1, Mention m2) {
+        if (m1.headWord().equals(m2.headWord()))
+            return true;
+        return false;
+    }
+
+    boolean sameLemma(Mention m1, Mention m2) {
+        if (m1.headToken().lemma().equals(m2.headToken().lemma()))
+            return true;
+        return false;
+    }
+
+    boolean sameNE(Mention m1, Mention m2) {
+        if (m1.headToken().nerTag().equals(m2.headToken().nerTag()))
+            return true;
+        return false;
+    }
+
+    boolean sameGenderOrUnknown(Mention m1, Mention m2) {
+        Pair<Boolean, Boolean> pair = Util.haveGenderAndAreSameGender(m1, m2);
+        if (pair.getSecond() && pair.getFirst())
+            return true;
+        if (!pair.getFirst())
+            return true;
+        return false;
+    }
+
+    boolean sameNumberOrUnknown(Mention m1, Mention m2) {
+        Pair<Boolean, Boolean> pair = Util.haveNumberAndAreSameNumber(m1, m2);
+        if (pair.getSecond() && pair.getFirst())
+            return true;
+        if (!pair.getFirst())
+            return true;
+        return false;
     }
 
 }
